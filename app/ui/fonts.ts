7fa7f03a03f98be476890c63ecb2f936e8f005dc
Rexskth <brajendrakaurav472@gmail.com>
2025-07-19 17:22:55 +0530

    committed successfully

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,12 +1,7 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 export const inter = Inter({ 
   subsets: ['latin'],
   display: 'swap' // optional but recommended
 });
 
-export const lusitana = Lusitana({
-  weight: ['400', '700'], // specify all weights you need
-  subsets: ['latin'],
-  display: 'swap' // optional but recommended
-});
